e54909a5af1bfd59d37dcc322684640b28c10490
Bruno Dantas <bruno96dantas@gmail.com>
2019-02-12 13:22:45 -0200

    Creating Listing micro service

---
+++ hashlab_listing/src/main/java/br/com/bruno96dantas/hashlab_listing/HashlabListingApplication.java
@@ -0,0 +1,14 @@
+package br.com.bruno96dantas.hashlab_listing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashlabListingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashlabListingApplication.class, args);
+	}
+
+}
+
---
+++ hashlab_listing/src/test/java/br/com/bruno96dantas/hashlab_listing/HashlabListingApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.bruno96dantas.hashlab_listing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HashlabListingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
